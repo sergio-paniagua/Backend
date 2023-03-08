abe5d9f0bccfd1799a5a1e770ca62a23162ce2c9
sergio-paniagua <sergiodpaniagua@gmail.com>
2023-03-08 01:21:46 -0300

    Ejercicio de clase terminado

--- ClasesBackend/class5/LecturaYEscrituraArchivos/index.js
+++ ClasesBackend/class5/LecturaYEscrituraArchivos/index.js
@@ -3,16 +3,28 @@ const fs = require ("fs")
 const {Blob} = require("buffer")  //investigar sobre esto, permite saber el tamaño del archivo
 
 const infoPackage = async () => {
-     //no tengo que crear el archivo porque ya existe (El JSON), solamente lo leo
-        const data = await fs.promises.readFile("./package.json", "utf-8")
-    //JSON.stringify() para cambiar de formato json a formato string
-    //JSON.parse() para cambiar del formato string a formato JSON
-    const info = {
-        contenidoSrt: JSON.stringify(data),
-        contenidoObj: JSON.parse(data),
-        size: new Blob([data]).size //tamaño del archivo
-    } 
-    console.log (info)
+    try {
+        //no tengo que crear el archivo porque ya existe (El JSON), solamente lo leo
+           const data = await fs.promises.readFile("./package.json", "utf-8")
+       //JSON.stringify() para cambiar de formato json a formato string
+       //JSON.parse() para cambiar del formato string a formato JSON
+       const info = {
+           contenidoSrt: JSON.stringify(data),
+           contenidoObj: JSON.parse(data),
+           size: new Blob([data]).size //tamaño del archivo
+       } 
+   
+       const {contenidoSrt, ...rest} = info;
+       const infojson ={
+       ...rest
+       }
+       console.log (info)
+       await fs.promises.appendFile("./info.json", JSON.stringify(infojson)); 
+       const newData = await fs.promises.readFile("./info.json", "utf-8") 
+       console.log(newData);
+        
+    } catch (error) {
+        console.log(error, "No se puede crear el archivo")
     }
-
+    }
 infoPackage()
