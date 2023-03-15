a9f3f70e8e97e9647ec9bab1e9909659857c6b4f
sergio-paniagua <sergiodpaniagua@gmail.com>
2023-03-15 00:10:04 -0300

    Correccion primer desafio

--- desafiosEntregables/DesafioEntregable1.js
+++ desafiosEntregables/DesafioEntregable1.js
@@ -1,63 +1,40 @@
 class ProductManager {
-  title = "";
-  description = "";
-  price;
-  thumbnail = "";
-  code;
-  stock = "";
-  id = 0;
-
-  constructor(title, description, price, thumbnail, code, stock, id) {
-    this.product = [];
-    this.title = title;
-    this.description = description;
-    this.price = price;
-    this.thumbnail = thumbnail;
-    this.code = code;
-    this.stock = stock;
-    this.id = id + 1;
+  constructor() {
+    //Se define en el metodo constructor un array que contendra los productos
+    //arranca vacio
+    this.products = [];
   }
 
-  addProduct (product) {
-    if (
-        !product.title,
-        !product.description,
-        !product.price,
-        !product.thumbnail,
-        !product.code,
-        !product.stock,
-        !product.id 
-      ) {
-        console.log("Completar todos los campos");
-      } else {
-        this.product.push(product);;
-      }
-    }
-    getProducts() {
-        return console.log(this.product);
-    }
- 
+  id = 0
 
-    getProductById(id){
-    this.product.find(idp => {console.log(idp)
-         if (this.id === id){
-            return console.log ("no valido")
-        } else console.log(id);
-    })};
-    
-   
+  //El metodo que agrega cada producto como un objeto
+  addProduct(title, description, price, thumbnail, code, stock, id) {
+    //hacemos una validacion
+    if (!title, !description, !price, !thumbnail, !code, !stock, !id){
+      console.log ("Debe completar todos los campos")
+    }else {
+      id++
+      this.products.push({title, description, price, thumbnail, code, stock, id});
+    }
+
+  }
+  getProducts() {
+    if(this.products.length == 0){
+      console.log ("No hay productos para mostrar")
+    }else {
+      console.log(this.products);
+    }
   }
 
-const producto1 = new ProductManager();
-const producto2 = new ProductManager("fideos", "kkkjk", 200, "./", 1, 200, 2);
-const producto3 = new ProductManager("fideos", "kkkjk", 200, "./", 1, 200, 3);
+  getProductById(id) {
+    if (!this.products.find((producto) => producto.id === id)){
+      return console.log ("no encontrado")
+    } else {
+      return console.log("existe")
+    }
+  }
+}
 
-producto2.addProduct(producto2);
-producto2.getProducts();
-producto2.getProductById(9);
-
-
-
-
-
+const productos = new ProductManager();
+productos.getProducts();
 
